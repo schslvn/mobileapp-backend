cad8f52797db188479e38e75f80deaa9f611f407
Silvin <silvin@student.umn.ac.id>
2018-07-04 23:11:39 +0700

    deleted unused import

--- src/main/java/umn/mobile/repository/MasterItemRepo.java
+++ src/main/java/umn/mobile/repository/MasterItemRepo.java
@@ -1,13 +1,11 @@
 package umn.mobile.repository;
 
-import javafx.scene.effect.SepiaTone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import umn.mobile.model.MasterItem;
 
 import java.util.List;
-import java.util.Set;
 
 public interface MasterItemRepo extends JpaRepository<MasterItem, Long> {
     @Query(value = "SELECT mi FROM MasterItem mi WHERE mi.item_name LIKE %:item_name%")
